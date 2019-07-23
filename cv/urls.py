7f40315d64c08c5fcca4cf67873efaf1cf96cbca
ha4219 <ha4219@naver.com>
2019-07-24 00:30:30 +0900

    face detection

--- cv/urls.py
+++ cv/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.first_view, name='first_view'),  # add
     url(r'^uimage/$', views.uimage, name='uimage'),
+    url(r'^dface/$', views.dface, name='dface'),  # add
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
--- cv/views.py
+++ cv/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from .form import UploadImageForm
 from django.core.files.storage import FileSystemStorage
+from .form import ImageUploadForm
+from django.conf import settings
+from .opencv_dface import opencv_dface
+
 # Create your views here.
 
 
@@ -25,3 +29,21 @@ def uimage(request):
         return render(request, 'cv/uimage.html', {
             'form': form
         })
+
+
+
+
+def dface(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+
+            imageURL = settings.MEDIA_URL + form.instance.document.name
+            opencv_dface(settings.MEDIA_ROOT_URL + imageURL)
+
+            return render(request, 'cv/dface.html', {'form': form, 'post': post})
+    else:
+        form = ImageUploadForm()
+    return render(request, 'cv/dface.html', {'form': form})
